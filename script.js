50ff34db35ba3d5695a18fc57cefdb35433bebb9
jdavis3333 <jdavis3333@gmail.com>
2020-06-28 17:07:19 -0700

    query URL and ajax pulling selectedCity are functional

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 var newCity;
+var cityHistory = [];
 
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-// var APIKey = "ef5a31cf3e6b80b9a71631a627c91754";
 
+
+
+
+//function to add rows to page and log history -- see 06-16
+function populateRows(arrayToUse, classToAdd, areaToAddTo) { 
+    $(areaToAddTo).empty();
+
+    for (var i = 0; i < cityHistory.length; i++) {
+      var newRow = $("<div>");
+      newRow.addClass(classToAdd);
+      newRow.attr("data-type", arrayToUse[i]);
+      newRow.text(arrayToUse[i]);
+      $(areaToAddTo).append(newRow);
+    }
+
+  }
 
 //event listener
 $("#button").click(function(event) {
@@ -11,18 +26,34 @@ $("#button").click(function(event) {
     newCity = $("#text").val().trim();  
     console.log(newCity);
     localStorage.setItem("newCity", newCity);
-    $("<#newDiv>").append(newCity);
+    newCity
+    
+    //ajax functionality
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=ef5a31cf3e6b80b9a71631a627c91754";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          // Log the queryURL
+          console.log(queryURL);
+          // Log the resulting object
+          console.log(response);
+          // Transfer content to HTML
+          $("#selectedCity").html("<h3>" + response.name + " Weather Details</h3>");
+          console.log(selectedCity)
+          var windSpeed = response.wind.speed;
+          $("#windSpeed").html("<p>Wind Speed: " +  windSpeed + "</p>");
+          $("#humidity").text("Humidity: " + response.main.humidity + "%");
+          $("#currentTemp").text("Temperature (F) " + response.main.temp);
+
+
+
+
+
+    })
+    //create an array, push each newCity into the array and append to newDiv
+    //see 06-08
+    // $("<#newDiv>").append(newCity);
 
 })
-
-
-
-
-
-
-//ajax functionality
-
-
-
-
-
