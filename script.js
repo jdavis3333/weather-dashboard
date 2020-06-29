ec3253e39cbbe913d2d38027b09dcb77d8987698
jdavis3333 <jdavis3333@gmail.com>
2020-06-29 16:06:01 -0700

    UV conditional coloring functional

--- script.js
+++ script.js
@@ -7,30 +7,29 @@ $(document).ready(function(){
     $("#currentDate").text(moment().format("dddd, MMMM Do YYYY"));
 
 
-    //function to add rows to page and log history -- see 06-16
-    function populateRows(arrayToUse, classToAdd, areaToAddTo) { 
-        $(areaToAddTo).empty();
-
-        for (var i = 0; i < cityHistory.length; i++) {
-        var newRow = $("<div>");
-        newRow.addClass(classToAdd);
-        newRow.attr("data-type", arrayToUse[i]);
-        newRow.text(arrayToUse[i]);
-        $(areaToAddTo).append(newRow);
-        }
-
-    }
-
     //event listener
     $("#button").click(function(event) {
         event.preventDefault();
-        //capture user input and store in local storage
+        //capture user input and store in local storage and array
         newCity = $("#text").val().trim();  
         console.log(newCity);
         localStorage.setItem("newCity", newCity);
-        newCity
-        
-    
+        cityHistory.push(newCity);
+        console.log(cityHistory);
+
+        // //recall from storage and display below search field
+        // function displayLocalStorage() {
+        //     for (let i = 0; i < cityHistory.length; i++) {
+        //         $("newCity").val(newCity.getItem(i));
+        //         var newRow = $("<div>");
+        //         newRow.addClass("row");
+        //         newRow.attr("data-name", cityHistory[i]);
+        //         newRow.text(cityHistry[i]);
+        //         $("historicalRows").append(newCity);
+        //     }
+        // }
+        // displayLocalStorage ();
+
 
         //ajax functionality
         //Search City
@@ -45,7 +44,7 @@ $(document).ready(function(){
                 // Log the resulting object
                 console.log(response);
                 // Transfer content to HTML
-                $("#selectedCity").html("<h3>" + response.name + " Weather Details</h3>");
+                $("#selectedCity").html("<h3>" + response.name + " </h3>");
                 //insert weather icon
                 var iconID = response.weather[0].icon;
                 var iconResult = "http://www.openweathermap.org/img/wn/" + iconID + "@2x.png";
@@ -64,15 +63,31 @@ $(document).ready(function(){
                 var long = response.coord.lon;
                 console.log(lat);
                 console.log(long);
+                //Add UV index with second ajax call
                 var queryURLUvIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=ef5a31cf3e6b80b9a71631a627c91754" + "&lat=" + lat + "&lon=" + long;
                     $.ajax({
                         url: queryURLUvIndex,
                         method: "GET"
                         }).then(function(response) {
                             $("#uvIndex").html("UV Index: " + response.value);
-                        ;
+                            //color code background depending on rating
+                            if (response.value < 3) {
+                               $("#uvIndex").css("background-color", "green");
+                            }else if (response.value < 5) {
+                               $("#uvIndex").css("background-color", "yellow");
+                            }else if (response.value < 7) {
+                                $("#uvIndex").css("background-color", "orange");
+                            }else if (response.value < 10) {
+                                $("#uvIndex").css("background-color", "red");
+                            }else if (response.value > 11) {
+                                $("#uvIndex").css("background-color", "violet");
+                            }                         
                     })
-            
+
+                //call function to create new rows
+                populateRows(cityHistory, "#historicalRows")
+
+
                 //5-day forecast 
                 // var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + newCity + "&appid=ef5a31cf3e6b80b9a71631a627c91754";
                 // console.log(fiveDayQueryURL);
@@ -90,7 +105,9 @@ $(document).ready(function(){
                     //     }
                     // };
                     // generateDates();/////////////
-                        // Log the resulting object
+
+
+                    // Log the resulting object
                     //       console.log(response);
                     //       // Transfer content to HTML
                     //       $(".cardDate").html("<h6>" + response.name + " Weather Details</h3>");
@@ -104,9 +121,8 @@ $(document).ready(function(){
             })
         
         
-        //create an array, push each newCity into the array and append to newDiv
-        //see 06-08
-        // $("<#newDiv>").append(newCity);
+ 
+
 
     })
 })    
