2c3d96e8b57bbd94046e0b6a08ac07a965a043dc
jdavis3333 <jdavis3333@gmail.com>
2020-06-28 23:37:48 -0700

    UV index query functional and displaying on page

--- script.js
+++ script.js
@@ -1,86 +1,112 @@
-var newCity;
-var cityHistory = [];
+$(document).ready(function(){
+    var newCity;
+    var cityHistory = [];
 
 
- //Day
-//  $("#currentDate").text(moment().format("dddd, MMMM Do YYYY"));
+    //Day
+    $("#currentDate").text(moment().format("dddd, MMMM Do YYYY"));
 
 
-//function to add rows to page and log history -- see 06-16
-function populateRows(arrayToUse, classToAdd, areaToAddTo) { 
-    $(areaToAddTo).empty();
+    //function to add rows to page and log history -- see 06-16
+    function populateRows(arrayToUse, classToAdd, areaToAddTo) { 
+        $(areaToAddTo).empty();
 
-    for (var i = 0; i < cityHistory.length; i++) {
-      var newRow = $("<div>");
-      newRow.addClass(classToAdd);
-      newRow.attr("data-type", arrayToUse[i]);
-      newRow.text(arrayToUse[i]);
-      $(areaToAddTo).append(newRow);
+        for (var i = 0; i < cityHistory.length; i++) {
+        var newRow = $("<div>");
+        newRow.addClass(classToAdd);
+        newRow.attr("data-type", arrayToUse[i]);
+        newRow.text(arrayToUse[i]);
+        $(areaToAddTo).append(newRow);
+        }
+
     }
 
-}
+    //event listener
+    $("#button").click(function(event) {
+        event.preventDefault();
+        //capture user input and store in local storage
+        newCity = $("#text").val().trim();  
+        console.log(newCity);
+        localStorage.setItem("newCity", newCity);
+        newCity
+        
+    
 
-//event listener
-$("#button").click(function(event) {
-    event.preventDefault();
-    //capture user input and store in local storage
-    newCity = $("#text").val().trim();  
-    console.log(newCity);
-    localStorage.setItem("newCity", newCity);
-    newCity
-    
-   
-
-    //ajax functionality
-    //Search City
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=ef5a31cf3e6b80b9a71631a627c91754";
-    
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
-          // Log the queryURL
-          console.log(queryURL);
-          // Log the resulting object
-          console.log(response);
-          // Transfer content to HTML
-          $("#selectedCity").html("<h3>" + response.name + " Weather Details</h3>");
-          var windSpeed = response.wind.speed;
-          $("#windSpeed").html("<p>Wind Speed: " +  windSpeed + " mph" + "</p>");
-          $("#humidity").text("Humidity: " + response.main.humidity + "%");
-          var kelvinTemp = response.main.temp;
-          var fahrenheit = Math.round(((kelvinTemp-273.15)*1.8)+32);
-          $("#currentTemp").text("Temperature (F) " + fahrenheit + "\u00B0");
-          var lat = response.coord.lat;
-          var long = response.coord.lon;
-          console.log(lat);
-          console.log(long);
-    
-        //5-day forecast 
-        var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=hourly,current,minutely&appid=ef5a31cf3e6b80b9a71631a627c91754"
-        console.log(fiveDayQueryURL);
+        //ajax functionality
+        //Search City
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=ef5a31cf3e6b80b9a71631a627c91754";
+        
         $.ajax({
-            url: fiveDayQueryURL,
+            url: queryURL,
             method: "GET"
-        }).then(function(response) {
-            // Log the queryURL
-            console.log(fiveDayQueryURL);
-            // Log the resulting object
-        //       console.log(response);
-        //       // Transfer content to HTML
-        //       $(".cardDate").html("<h6>" + response.name + " Weather Details</h3>");
-        //       var windSpeed = response.wind.speed;
-        //       $("#windSpeed").html("<p>Wind Speed: " +  windSpeed + " mph" + "</p>");
-        //       $("#humidity").text("Humidity: " + response.main.humidity + "%");
-        //       var kelvinTemp = response.main.temp;
-        //       var fahrenheit = Math.round(((kelvinTemp-273.15)*1.8)+32);
-        //       $("#currentTemp").text("Temperature (F) " + fahrenheit + "\u00B0");
+            }).then(function(response) {
+                // Log the queryURL
+                console.log(queryURL);
+                // Log the resulting object
+                console.log(response);
+                // Transfer content to HTML
+                $("#selectedCity").html("<h3>" + response.name + " Weather Details</h3>");
+                //insert weather icon
+                var iconID = response.weather[0].icon;
+                var iconResult = "http://www.openweathermap.org/img/wn/" + iconID + "@2x.png";
+                $("#weatherIcon").attr("src", iconResult);
+                //insert windspeed
+                var windSpeed = response.wind.speed;
+                $("#windSpeed").html("<p>Wind Speed: " +  windSpeed + " mph" + "</p>");
+                //insert humidity
+                $("#humidity").text("Humidity: " + response.main.humidity + "%");
+                //insert temp
+                var kelvinTemp = response.main.temp;
+                var fahrenheit = Math.round(((kelvinTemp-273.15)*1.8)+32);
+                $("#currentTemp").text("Temperature (F) " + fahrenheit + "\u00B0");
+                //find lat and long
+                var lat = response.coord.lat;
+                var long = response.coord.lon;
+                console.log(lat);
+                console.log(long);
+                var queryURLUvIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=ef5a31cf3e6b80b9a71631a627c91754" + "&lat=" + lat + "&lon=" + long;
+                    $.ajax({
+                        url: queryURLUvIndex,
+                        method: "GET"
+                        }).then(function(response) {
+                            $("#uvIndex").html("UV Index: " + response.value);
+                        ;
+                    })
+            
+                //5-day forecast 
+                // var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + newCity + "&appid=ef5a31cf3e6b80b9a71631a627c91754";
+                // console.log(fiveDayQueryURL);
+                // $.ajax({
+                //     url: fiveDayQueryURL,
+                //     method: "GET"
+                // }).then(function(response) {
+                //     // Log the queryURL
+                //     console.log(fiveDayQueryURL);////
+                    // function generateDates() {
+                    //     for (i = 0; i < 5; i + 1){
+                    //     var dates5day = moment().add(i+1,'day').format("M/D/YYYY"); 
+                    //     console.log(dates5day);
+                    //     $("#card" + i.toString()).text(dates5day); 
+                    //     }
+                    // };
+                    // generateDates();/////////////
+                        // Log the resulting object
+                    //       console.log(response);
+                    //       // Transfer content to HTML
+                    //       $(".cardDate").html("<h6>" + response.name + " Weather Details</h3>");
+                    //       var windSpeed = response.wind.speed;
+                    //       $("#windSpeed").html("<p>Wind Speed: " +  windSpeed + " mph" + "</p>");
+                    //       $("#humidity").text("Humidity: " + response.main.humidity + "%");
+                    //       var kelvinTemp = response.main.temp;
+                    //       var fahrenheit = Math.round(((kelvinTemp-273.15)*1.8)+32);
+                    //       $("#currentTemp").text("Temperature (F) " + fahrenheit + "\u00B0");
+                
+            })
+        
+        
+        //create an array, push each newCity into the array and append to newDiv
+        //see 06-08
+        // $("<#newDiv>").append(newCity);
 
-        })
     })
-
-    //create an array, push each newCity into the array and append to newDiv
-    //see 06-08
-    // $("<#newDiv>").append(newCity);
-
-})
+})    
